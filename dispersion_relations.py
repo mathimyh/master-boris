4b511db5dc040e3d30fcaac7749b59d16fa0f990
Mathias Myhre <matmyhre@outlook.com>
2025-03-04 11:25:36 +0100

    fix dispersion during steady state, add FM compatibility

--- dispersion_relations.py
+++ dispersion_relations.py
@@ -12,9 +12,9 @@ import transport
 
 path = 'C:/Users/mathimyh/master/master-boris/'
 
-def magnon_dispersion(meshdims, cellsize, t, V, damping, MEC, ani, T, type, dir, axis, steadystate = False):
+def magnon_dispersion(ns, meshdims, cellsize, t, V, damping, MEC, ani, T, type, dir, axis, steadystate = False):
 
-    ns = NSClient(); ns.configure(True, False)
+    # # ns = NSClient(); ns.configure(True, False)
 
     int_dir = 0
 
@@ -37,31 +37,43 @@ def magnon_dispersion(meshdims, cellsize, t, V, damping, MEC, ani, T, type, dir,
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
-    time_step = 0.1e-12
-    total_time = t*1e-12
+    if type == 'AFM':
+        time_step = 0.1e-12
+        total_time = t*1e-12
+    elif type == 'FM':
+        time_step = 1e-12
+        total_time = t*1e-12
 
     Ms = 2.1e3
+
+    y_vals = [25, 5, 45]
     
     if steadystate:
         sim_name = path + type + '/' + modules_folder + ani + '/sims/' + str(meshdims[0]) + 'x' + str(meshdims[1]) + 'x' + str(meshdims[2]) + '/V' + str(V) + '_damping' + str(damping) + '_' + str(T) + 'K_steady_state.bsm'
         ns.loadsim(sim_name)
-        output_file = path + type + '/' + modules_folder + ani + '/cache/dispersions/' + str(meshdims[0]) + 'x' + str(meshdims[1]) + 'x' + str(meshdims[2]) +  '/' + 'dir' + dir + '_axis' + axis + 'V' + str(V) + '_damping' + str(damping) + '_T' + str(T) +  '_dispersion.txt'
+        output_file1 = path + type + '/' + modules_folder + ani + '/cache/dispersions/' + str(meshdims[0]) + 'x' + str(meshdims[1]) + 'x' + str(meshdims[2]) +  '/' + 'dir' + dir + '_axis' + axis + 'V' + str(V) + '_damping' + str(damping) + '_T' + str(T) + '_y=' + str(y_vals[0]) + '_dispersion.txt'
+        output_file2 = path + type + '/' + modules_folder + ani + '/cache/dispersions/' + str(meshdims[0]) + 'x' + str(meshdims[1]) + 'x' + str(meshdims[2]) +  '/' + 'dir' + dir + '_axis' + axis + 'V' + str(V) + '_damping' + str(damping) + '_T' + str(T) + '_y=' + str(y_vals[1]) + '_dispersion.txt'
+        output_file3 = path + type + '/' + modules_folder + ani + '/cache/dispersions/' + str(meshdims[0]) + 'x' + str(meshdims[1]) + 'x' + str(meshdims[2]) +  '/' + 'dir' + dir + '_axis' + axis + 'V' + str(V) + '_damping' + str(damping) + '_T' + str(T) + '_y=' + str(y_vals[2]) + '_dispersion.txt'
+        output_files = [output_file1, output_file2, output_file3]
+
     else:
         if type == 'AFM':
             M = transport.Init_AFM(meshdims, cellsize, damping, MEC, ani, T)
         elif type == 'FM':
-            M = 0
+            M = transport.Init_FM(meshdims, cellsize, damping, MEC, ani, T)
         else:
             print('Choose type!')
             exit()
         output_file = path + type + '/' + modules_folder + ani + '/cache/dispersions/' + str(meshdims[0]) + 'x' + str(meshdims[1]) + 'x' + str(meshdims[2]) +  '/' + 'dir' + dir + '_axis' + axis + 'groundstate' + '_damping' + str(damping) + '_T' + str(T) +  '_dispersion.txt'
+        output_files = [output_file]
 
     ns.reset()
 
     time = 0.0
     ns.cuda(1)
 
-    ns.dp_newfile(output_file)
+    for output_filex in output_files:
+        ns.dp_newfile(output_filex)
 
     while time < total_time:
         
@@ -70,16 +82,16 @@ def magnon_dispersion(meshdims, cellsize, t, V, damping, MEC, ani, T, type, dir,
         else:
             ns.Relax(['time', time + time_step])
         
-        if axis == 'x':
-            ns.dp_getexactprofile((np.array([cellsize/2, 0, meshdims[2]-cellsize])*1e-9), (np.array([meshdims[0] - cellsize/2, meshdims[1], meshdims[2]])*1e-9), cellsize*1e-9, 0)
-        elif axis == 'z':
-            ns.dp_getexactprofile((np.array([meshdims[0]/2-cellsize/2, meshdims[1]/2-cellsize/2, meshdims[2]-cellsize/2])*1e-9), (np.array([meshdims[0]/2-cellsize/2, meshdims[1]/2-cellsize/2, 40+cellsize/2])*1e-9), cellsize*1e-9, 0)
-        ns.dp_div(int_dir, Ms)
-        ns.dp_saveappendasrow(output_file, int_dir)
+        for i, output_filex in enumerate(output_files):
+            # if axis == 'x':
+            ns.dp_getexactprofile((np.array([cellsize/2, y_vals[i], meshdims[2]-cellsize])*1e-9), (np.array([meshdims[0] - cellsize/2, y_vals[i], meshdims[2]])*1e-9), cellsize*1e-9, 0)
+            # elif axis == 'z':
+            #     ns.dp_getexactprofile((np.array([meshdims[0]/2-cellsize/2, meshdims[1]/2-cellsize/2, meshdims[2]-cellsize/2])*1e-9), (np.array([meshdims[0]/2-cellsize/2, meshdims[1]/2-cellsize/2, 40+cellsize/2])*1e-9), cellsize*1e-9, 0)
+            ns.dp_div(int_dir, Ms)
+            ns.dp_saveappendasrow(output_filex, int_dir)
         time += time_step
 
     plotting.plot_magnon_dispersion(meshdims, cellsize, t, V, damping, MEC, ani, T, type, dir, axis, steadystate)
-
 
 def phonon_dispersion(meshdims, cellsize, t, damping, x_start, x_stop, MEC, ani, dir):
 
@@ -189,27 +201,24 @@ def trajectory(meshdims, t, damping, x_start, x_stop, MEC, ani, dir):
 
     plotting.plot_trajectory(meshdims, damping, MEC, ani, dir)
 
-def critical_T(meshdims, cellsize, t, damping, MEC, ani, type, max_T):
+def critical_T(ns, meshdims, cellsize, t, damping, MEC, ani, type, max_T):
     modules_folder = 'ex+ani'
     if MEC:
         modules_folder += 'mec'
     modules_folder += '/'
     
-
     folder_name = type + '/' + modules_folder + ani + '/cache/critical_T/' + str(meshdims[0]) + 'x' + str(meshdims[1]) + 'x' + str(meshdims[2])
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
-
-    time_step = 0.1e-12
-
-    Ms = 2.1e3
-
-    ns = NSClient(); ns.configure(True, False)
     
     if type == 'AFM':
-        M = transport.Init_AFM(meshdims, cellsize, damping, MEC, ani, type)
+        M = transport.Init_AFM(meshdims, cellsize, damping, MEC, ani, 0)
+        measuring_t = 10e-12
+        step_t = 40e-12
     elif type == 'FM':
-        M = 0
+        M = transport.Init_FM(meshdims, cellsize, damping, MEC, ani, 0)
+        measuring_t = 100e-12
+        step_t = 400e-12
     else:
         print('Choose material type!')
         exit()
@@ -231,14 +240,14 @@ def critical_T(meshdims, cellsize, t, damping, MEC, ani, type, max_T):
     # Increase temperature over time 
     for i in range(max_T):
         ns.temperature(i)
-        ns.Relax(['time', 40e-12])
+        ns.Relax(['time', step_t])
         m = [0,0,0]
         for j in range(avs):
-            temp = ns.showdata('<M2>', type, np.array([0,0,0,meshdims[0],meshdims[1],meshdims[2]])*1e-9)
+            temp = ns.showdata('<M>', type, np.array([0,0,0,meshdims[0],meshdims[1],meshdims[2]])*1e-9)
             m[0] += temp[0]
             m[1] += temp[1]
             m[2] += temp[2]
-            ns.Relax(['time', 10e-12])
+            ns.Relax(['time', measuring_t])
             ns.reset()
         m[0] /= avs
         m[1] /= avs
